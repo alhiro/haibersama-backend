db0984249c00800fea5d60168e73efd6d1075fec
riawaryati <iyha25@gmail.com>
2020-03-28 19:40:14 +0700

    add order validation

--- services/reservation.js
+++ services/reservation.js
@@ -25,6 +25,14 @@ module.exports =
           };
         }
 
+        if(package.partner_id != partnerId){
+          return {
+            success: false,
+            message: "Partner tidak bisa menggunakan package yang dipilih.",
+            data: {}
+          };
+        }
+
         //check duplicate reservation user with event date, event time, partner id 
         const params = {
           user_id: userId,
