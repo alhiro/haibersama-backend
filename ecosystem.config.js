41b0c8eff50cd0e6f81ced5438d1c245c238bdf0
lvj <lvj@lvjs-iMac.local>
2025-06-07 12:43:53 +0700

    update pm2 ecosystem port

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
            script: './app.js',
            env: {
               NODE_ENV: 'production',
-              PORT: 3000
+              PORT: 3001
            }
        }
     ],
